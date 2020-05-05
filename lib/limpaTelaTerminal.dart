ecdac4cdb6ae996274b4135d61bb4cf83cfc9cc5
Oston Prata <washingtonferreiraprata@gmail.com>
2020-05-05 20:07:12 -0300

    Add files via upload

---
+++ lib/limpaTelaTerminal.dart
@@ -0,0 +1,24 @@
+/*
+  Oston Prata , Brasília 05 De Maior De 2020.
+
+  CONTATO: 
+  ostonprata@gmail.com
+
+  CÓDIGOS FONTES:
+  https://github.com/ostonprata
+
+  REDES SOCIAIS:
+  https://www.youtube.com/channel/UCHbB7TRl0WNytk9Xx1jxXOQ
+  https://twitter.com/ostonprata
+  https://www.instagram.com/ostonprata/
+
+  EXECUÇÃO:
+  dart calculadoraPopular/bin/main.dart
+  calculadoraPopular.exe
+*/
+
+// Função para limpar a tela do terminal
+void limpaTelaTerminal()
+{
+  print("\x1B[2J\x1B[0;0H");
+}
